15452299eceec0945b87224ae96bab16d386bcae
Stephan <stephan@inkmi.com>
2023-02-28 07:16:07 +0100

    Add test

---
+++ internal/source_test.go
@@ -0,0 +1,31 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestPrintSource(t *testing.T) {
+	tp := NewTestPrinter()
+	Printer = TestPrinterFunc(tp)
+	lines := strings.Split(`func test() {
+x = 4
+y = 3
+err := add(x,y)
+Errorf("x", err, x, y)
+}
+`, "\n")
+	data := PrintSourceOptions{
+		DebugLine:     4,
+		ShortFileName: getShortFilePath("error.go"),
+		FailingLine:   3,
+		FuncLine:      0,
+		Highlighted:   map[int][]int{},
+		StartLine:     0,
+		EndLine:       len(lines),
+		UsedVars:      []UsedVar{},
+	}
+	printSource(lines, data)
+	assert.Equal(t, 7, len(tp.Output))
+}
