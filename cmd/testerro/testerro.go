f8bb28524b8d82adf604afc4aab110354d5e64aa
Stephan Schmidt <stephan.schmidt@gmail.com>
2024-05-11 12:17:32 +0200

    Change to command line tool

--- cmd/testerro/testerro.go
+++ cmd/testerro/testerro.go
@@ -9,10 +9,14 @@ import (
 func main() {
 	logger := log.With().Caller().Logger()
 
+	logger.Info().Msg("Welcome to erro 🧑‍🚀")
+	logger.Info().Msg("https://github.com/inkmi/erro")
 	logger.Info().Int("Hello", 42).Msg("Info example")
+	logger.Trace().Int("Hello", 23).Msg("Trace example")
 	logger.Debug().Str("Hello", "🦄").Msg("Debug example")
 	logger.Warn().Str("Hello", "World").Msg("Warn example")
 
 	err := errors.New("Testerror")
 	logger.Error().Err(err).Str("Test", "Test").Msg("Error example")
+	logger.Info().Int("After", 1).Int("Days", 2).Msg("After the error")
 }
