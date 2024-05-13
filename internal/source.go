14d868e763e60b8140f44b029ece0aeb2aeddb7b
Stephan Schmidt <stephan.schmidt@gmail.com>
2024-05-13 04:59:06 +0200

    Beautify output.

--- internal/source.go
+++ internal/source.go
@@ -27,22 +27,22 @@ func printSource(lines []string, opts PrintSourceOptions) {
 		}
 	}
 
-	if len(filteredVars) > 0 {
-		printf("    %s:%d %s\n", opts.ShortFileName, filteredVars[0].LastWrite, color.RedString(filteredVars[0].SourceLastWrite))
-
-		// Iterate over the slice starting from the second item
-		for i := 1; i < len(filteredVars); i++ {
-			// Check if there is a gap between the current and the previous item
-			if filteredVars[i].LastWrite > filteredVars[i-1].LastWrite+1 {
-				println(color.YellowString("..."))
-			}
-			printf("    %s:%d %s\n", opts.ShortFileName, filteredVars[i].LastWrite, color.RedString(filteredVars[i].SourceLastWrite))
-		}
-
-		if filteredVars[len(filteredVars)-1].LastWrite < opts.FailingLine {
-			printf("    %s\n", color.YellowString("..."))
-		}
-	}
+	//if len(filteredVars) > 0 {
+	//	printf("    %s:%d %s\n", opts.ShortFileName, filteredVars[0].LastWrite, color.RedString(filteredVars[0].SourceLastWrite))
+	//
+	//	// Iterate over the slice starting from the second item
+	//	for i := 1; i < len(filteredVars); i++ {
+	//		// Check if there is a gap between the current and the previous item
+	//		if filteredVars[i].LastWrite > filteredVars[i-1].LastWrite+1 {
+	//			println(color.YellowString("..."))
+	//		}
+	//		printf("    %s:%d %s\n", opts.ShortFileName, filteredVars[i].LastWrite, color.RedString(filteredVars[i].SourceLastWrite))
+	//	}
+	//
+	//	if filteredVars[len(filteredVars)-1].LastWrite < opts.FailingLine {
+	//		printf("    %s\n", color.YellowString("..."))
+	//	}
+	//}
 
 	// Last print the failing line
 	printf("    %s:%d %s\n", opts.ShortFileName, opts.FailingLine+1, color.RedString(strings.TrimSpace(lines[opts.FailingLine])))
